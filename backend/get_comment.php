7c80252a73ff73d0d2f31fc99a36501fedb83fa1
nttouyen28 <nguyenthitouyenxc@gmail.com>
2023-12-13 23:21:04 +0700

    ham lay thong tin: cmt va hotels

--- backend/get_comment.php
+++ backend/get_comment.php
@@ -1,34 +1,53 @@
 <?php
+
 include 'condb.php';
 
-if (isset($_GET['hotel_id'])){
-    $hotel_id = $_GET['hotel_id'];
-    $sql = "SELECT * FROM user JOIN comment ON user.user_id = comment.user_id WHERE `hotel_id` = $hotel_id";
-} elseif (isset($_GET['blog_id'])) {
-    $blog_id = $_GET['blog_id'];
-    $sql = "SELECT * FROM user JOIN comment ON user.user_id = comment.user_id WHERE `blog_id` = $blog_id";
-} else {
-    $sql = "SELECT comment.*, hotel.name AS hotel_name, user.name AS user_name, blog.title AS blog_title, hotel.img AS hotel_img, blog.img AS blog_img FROM comment 
-    JOIN user ON user.user_id = comment.user_id 
-    LEFT JOIN hotel ON hotel.id = comment.hotel_id 
-    LEFT JOIN blog ON blog.id= comment.blog_id;";
-}
+class CommentController
+{
+    private $conn;
 
-
-$result = $conn->query($sql);
-$data = array();
-    if ($result->num_rows > 0) {
-        while ($row = $result->fetch_assoc()) {
-            $data[] = $row;
-        }
-    } else {
-        echo "Không có dữ liệu";
+    public function __construct($conn)
+    {
+        $this->conn = $conn;
     }
 
-// Trả về dữ liệu dưới dạng JSON
-header('Content-Type: application/json');
-echo json_encode($data);
+    public function getCommentData()
+    {
+        $hotel_id = isset($_GET['hotel_id']) ? $_GET['hotel_id'] : null;
+        $blog_id = isset($_GET['blog_id']) ? $_GET['blog_id'] : null;
 
-// Đóng kết nối đến cơ sở dữ liệu
-$result->close();
+        if ($hotel_id !== null) {
+            $sql = "SELECT * FROM user JOIN comment ON user.user_id = comment.user_id WHERE `hotel_id` = $hotel_id";
+        } elseif ($blog_id !== null) {
+            $sql = "SELECT * FROM user JOIN comment ON user.user_id = comment.user_id WHERE `blog_id` = $blog_id";
+        } else {
+            $sql = "SELECT comment.*, hotel.name AS hotel_name, user.name AS user_name, blog.title AS blog_title, hotel.img AS hotel_img, blog.img AS blog_img FROM comment 
+                    JOIN user ON user.user_id = comment.user_id 
+                    LEFT JOIN hotel ON hotel.id = comment.hotel_id 
+                    LEFT JOIN blog ON blog.id = comment.blog_id;";
+        }
+
+        $result = $this->conn->query($sql);
+        $data = array();
+
+        if ($result->num_rows > 0) {
+            while ($row = $result->fetch_assoc()) {
+                $data[] = $row;
+            }
+        } else {
+            echo "Không có dữ liệu";
+        }
+
+        // Trả về dữ liệu dưới dạng JSON
+        header('Content-Type: application/json');
+        echo json_encode($data);
+
+        // Đóng kết nối đến cơ sở dữ liệu
+        $result->close();
+    }
+}
+
+// Sử dụng class CommentController
+$commentController = new CommentController($conn);
+$commentController->getCommentData();
 ?>
--- backend/get_hotels.php
+++ backend/get_hotels.php
@@ -1,41 +1,66 @@
 <?php
+
 include 'condb.php';
 
-if (isset($_GET['id'])){
-    $id = $_GET['id'];
-    $sql = "SELECT * FROM hotel WHERE id = $id";
-} elseif(isset($_GET['city'])) {
-    $city = $_GET['city'];
-    $type = $_GET['type'];
-    if ($city == "Tất cả" && $type == "Tất cả")
-        $sql = "SELECT * FROM hotel";
-    elseif ($city == "Tất cả" && $type != "Tất cả")
-        $sql = "SELECT * FROM hotel WHERE `type` = '$type'";
-    elseif ($city != "Tất cả" && $type == "Tất cả")
-        $sql = "SELECT * FROM hotel WHERE city = '$city'";
-    else
-        $sql = "SELECT * FROM hotel WHERE city = '$city' AND 'type' = '$type'";
-} else {
-    $sql = "SELECT hotel.*,COUNT(comment.star) AS star_quantity, SUM(comment.star) AS star_total, IFNULL(AVG(comment.star), 0) AS star_avg
-    FROM hotel 
-    LEFT JOIN comment ON hotel.id = comment.hotel_id GROUP BY hotel.id;";
-}
+class HotelController
+{
+    private $conn;
 
-
-$result = $conn->query($sql);
-$data = array();
-    if ($result->num_rows > 0) {
-        while ($row = $result->fetch_assoc()) {
-            $data[] = $row;
-        }
-    } else {
-        echo "Không có dữ liệu";
+    public function __construct($conn)
+    {
+        $this->conn = $conn;
     }
 
-// Trả về dữ liệu dưới dạng JSON
-header('Content-Type: application/json');
-echo json_encode($data);
+    public function getHotelData()
+    {
+        $id = isset($_GET['id']) ? $_GET['id'] : null;
+        $city = isset($_GET['city']) ? $_GET['city'] : null;
+        $type = isset($_GET['type']) ? $_GET['type'] : null;
 
-// Đóng kết nối đến cơ sở dữ liệu
-$result->close();
+        if ($id !== null) {
+            $sql = "SELECT * FROM hotel WHERE id = $id";
+        } elseif ($city !== null) {
+            $sql = $this->buildCityTypeQuery($city, $type);
+        } else {
+            $sql = "SELECT hotel.*, COUNT(comment.star) AS star_quantity, SUM(comment.star) AS star_total, IFNULL(AVG(comment.star), 0) AS star_avg
+                    FROM hotel 
+                    LEFT JOIN comment ON hotel.id = comment.hotel_id GROUP BY hotel.id;";
+        }
+
+        $result = $this->conn->query($sql);
+        $data = array();
+
+        if ($result->num_rows > 0) {
+            while ($row = $result->fetch_assoc()) {
+                $data[] = $row;
+            }
+        } else {
+            echo "Không có dữ liệu";
+        }
+
+        // Trả về dữ liệu dưới dạng JSON
+        header('Content-Type: application/json');
+        echo json_encode($data);
+
+        // Đóng kết nối đến cơ sở dữ liệu
+        $result->close();
+    }
+
+    private function buildCityTypeQuery($city, $type)
+    {
+        if ($city == "Tất cả" && $type == "Tất cả") {
+            return "SELECT * FROM hotel";
+        } elseif ($city == "Tất cả" && $type != "Tất cả") {
+            return "SELECT * FROM hotel WHERE `type` = '$type'";
+        } elseif ($city != "Tất cả" && $type == "Tất cả") {
+            return "SELECT * FROM hotel WHERE city = '$city'";
+        } else {
+            return "SELECT * FROM hotel WHERE city = '$city' AND `type` = '$type'";
+        }
+    }
+}
+
+// Sử dụng class HotelController
+$hotelController = new HotelController($conn);
+$hotelController->getHotelData();
 ?>
