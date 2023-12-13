c6a30ddff6b89a06b599f756fc24d8eb13841bb3
lavadayne <levietanh09022003@gmail.com>
2023-12-13 23:18:22 +0700

    ham lay thong tin: user, booking, blogs

--- backend/get_blogs.php
+++ backend/get_blogs.php
@@ -1,61 +1,79 @@
 <?php
+
 include 'condb.php';
 
-if (isset($_GET['type']) AND $_GET['type'] != 'all'){
-    $type = $_GET['type'];
-    $sql = "SELECT blog.*, COUNT(comment.star) AS star_quantity, SUM(comment.star) AS star_total
-    FROM blog
-    LEFT JOIN comment ON blog.id = comment.blog_id 
-    WHERE `type` = '$type'
-    GROUP BY blog.id;";
-} else {
-    $sql = "SELECT blog.*,COUNT(comment.star) AS star_quantity, SUM(comment.star) AS star_total
-    FROM blog 
-    LEFT JOIN comment ON blog.id = comment.blog_id GROUP BY blog.id;";
-}
+class BlogController
+{
+    private $conn;
 
-if (isset($_GET['search'])){
-    $search = $_GET['search'];
-    $sql = "SELECT blog.*, COUNT(comment.star) AS star_quantity, SUM(comment.star) AS star_total
-    FROM blog
-    LEFT JOIN comment ON blog.id = comment.blog_id
-    WHERE blog.title LIKE '%$search%'
-    GROUP BY blog.id;
-    ";
-}
-
-if (isset($_GET['id'])){
-    $id = $_GET['id'];
-    $sql = "SELECT * FROM blog WHERE `id` = '$id'";
-}
-
-if (isset($_GET['popular'])){
-    $sql = "SELECT blog.*, AVG(star) AS star_avg
-    FROM blog 
-    LEFT JOIN comment ON blog.id = comment.blog_id
-    GROUP BY blog.id
-    HAVING star_avg IS NOT NULL
-    ORDER BY star_avg DESC";
-}
-
-if (isset($_GET['recent'])){
-    $sql = "SELECT * FROM blog ORDER BY id DESC";
-}
-
-$result = $conn->query($sql);
-$data = array();
-    if ($result->num_rows > 0) {
-        while ($row = $result->fetch_assoc()) {
-            $data[] = $row;
-        }
-    } else {
-        echo "Không có dữ liệu";
+    public function __construct($conn)
+    {
+        $this->conn = $conn;
     }
 
-// Trả về dữ liệu dưới dạng JSON
-header('Content-Type: application/json');
-echo json_encode($data);
+    public function getBlogData()
+    {
+        $sql = $this->buildQuery();
 
-// Đóng kết nối đến cơ sở dữ liệu
-$result->close();
+        $result = $this->conn->query($sql);
+        $data = array();
+
+        if ($result->num_rows > 0) {
+            while ($row = $result->fetch_assoc()) {
+                $data[] = $row;
+            }
+        } else {
+            echo "Không có dữ liệu";
+        }
+
+        // Trả về dữ liệu dưới dạng JSON
+        header('Content-Type: application/json');
+        echo json_encode($data);
+
+        // Đóng kết nối đến cơ sở dữ liệu
+        $result->close();
+    }
+
+    private function buildQuery()
+    {
+        $type = isset($_GET['type']) && $_GET['type'] !== 'all' ? $_GET['type'] : null;
+        $search = isset($_GET['search']) ? $_GET['search'] : null;
+        $id = isset($_GET['id']) ? $_GET['id'] : null;
+
+        if ($type !== null) {
+            return "SELECT blog.*, COUNT(comment.star) AS star_quantity, SUM(comment.star) AS star_total
+                FROM blog
+                LEFT JOIN comment ON blog.id = comment.blog_id 
+                WHERE `type` = '$type'
+                GROUP BY blog.id;";
+        } elseif ($search !== null) {
+            return "SELECT blog.*, COUNT(comment.star) AS star_quantity, SUM(comment.star) AS star_total
+                FROM blog
+                LEFT JOIN comment ON blog.id = comment.blog_id
+                WHERE blog.title LIKE '%$search%'
+                GROUP BY blog.id;";
+        } elseif ($id !== null) {
+            return "SELECT * FROM blog WHERE `id` = '$id'";
+        } elseif (isset($_GET['popular'])) {
+            return "SELECT blog.*, AVG(star) AS star_avg
+                FROM blog 
+                LEFT JOIN comment ON blog.id = comment.blog_id
+                GROUP BY blog.id
+                HAVING star_avg IS NOT NULL
+                ORDER BY star_avg DESC";
+        } elseif (isset($_GET['recent'])) {
+            return "SELECT * FROM blog ORDER BY id DESC";
+        }
+
+        // Default query (if no condition is met)
+        return "SELECT blog.*, COUNT(comment.star) AS star_quantity, SUM(comment.star) AS star_total
+            FROM blog 
+            LEFT JOIN comment ON blog.id = comment.blog_id 
+            GROUP BY blog.id;";
+    }
+}
+
+// Sử dụng class BlogController
+$blogController = new BlogController($conn);
+$blogController->getBlogData();
 ?>
